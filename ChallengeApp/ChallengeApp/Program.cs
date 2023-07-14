61cdc2c158993eb877402f22d5c46bd141663870
Elizzka <elizzakapela@gmail.com>
2023-07-14 12:21:27 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var gender = "Kobieta";
+var age = 28;
+
+if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+
+var name = "Ewa";
+var age2 = 33;
+
+if (name == "Ewa" && age2 == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+
+var gender2 = "Mężczyzna";
+var age3 = 17;
+
+if (gender2 == "Mężczyzna" && age3 < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
+
